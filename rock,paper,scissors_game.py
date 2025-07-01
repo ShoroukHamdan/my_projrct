0962a540f96dddf72e6d82a546dd110067364373
shorouk <shoruokhamdan@gmail.com>
2025-07-01 21:33:04 +0300

    عدلت لعبة حجر ورقة مقص في الفرع new-feature

--- rock,paper,scissors_game.py
+++ rock,paper,scissors_game.py
@@ -1,5 +1,29 @@
+print("Hello from the new feature branch!")
 import random
-print ("Welcom to the Rock,Paper,Scissors Game:")
+rock_ascii= """
+    ______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___) 
+"""
+paper_ascii=  """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)  
+    """
+scissors_ascii="""
+      _____
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___) 
+    """
 Input= input("press Enter to continue or press (Help) for the rules help: ").capitalize()
 if Input == "Help":
     print ("""
@@ -11,102 +35,36 @@ if Input == "Help":
            """)
 user_choice =input("Enter your choice (rock,paper,scissors): ").lower()
 if user_choice=="rock":
-    print("""
-you choose 
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)       
-          """)
+    print(f"your choice:\n{rock_ascii}")
 elif user_choice=="paper":
-    print("""
-    you choose
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)      
-          """)
+  print(f"your choice:\n{paper_ascii}")
+
 elif user_choice=="scissors":
-    print("""
-     you choose 
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)     
-          """)
-    
+    print(f"your choice:\n {scissors_ascii}")
 else :
-    print("unvalid choice")
+    print ("unvalid choice")
     
 choices =["rock","paper","scissors"]
-
-index=random.randint(0,2)
-computer_choice=choices[index]
+computer_choice=random.choice(choices)
 
 if computer_choice=="rock":
-    print("""
-computer choose 
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)       
-          """)
+    print(f"computer choice\n{rock_ascii}")
 elif computer_choice=="paper":
-    print("""
-    computer choose
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)      
-          """)
-elif computer_choice=="scissors":
-    print("""
-     computer choose 
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)     
-          """)
-    
-else :
-    print("unvalid choice")
-    
-if user_choice!=computer_choice:
-    if user_choice=="paper" and computer_choice=="rock":
-        print ("you win! paper cover the rock")
-     
-    if user_choice=="rock" and computer_choice=="scissors":
-        print ("you win! rock smashe the scissors")
-      
-    if user_choice=="scissors" and computer_choice=="paper":
-        print("you win! scissors beats the paper ")    
-        
-        
-       
-    if user_choice=="paper" and computer_choice=="scissors":
-          print("you lose! scissors beats the paper")
-        
-    if user_choice=="rock" and computer_choice=="paper":
-         print("you lose! paper beats the rock")
-         
-    if user_choice=="scissors" and computer_choice=="rock":     
-         print("you lose! rock smashe the scissors ")
-        
-else :
-    print("you are equal to the computer")
-        
-    
+  print(f"computer choice\n {paper_ascii}")
+
+else:
+    print(f"computer choice\n {scissors_ascii}")
 
 
+if( ( user_choice=="paper" and computer_choice=="rock") 
+    or
+   ( user_choice=="rock" and computer_choice=="scissors") 
+      or
+  (user_choice=="scissors" and computer_choice=="paper")
+   ):
+      print(f"you win!{user_choice} beats {computer_choice}")
+elif user_choice == computer_choice:
+     print("it is a tie!")
+else :
+      print(f"You lose! {computer_choice} beats {user_choice}")
+    
